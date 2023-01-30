c769997560708249647c20cff024aa07226f37ad
Yunhee Kim <yhstory@biztechi.com>
2023-01-30 14:17:36 +0900

    init mvn project

---
+++ src/main/java/com/example/mvndockerapp/MvnDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvndockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnDockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnDockerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvndockerapp/MvnDockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvndockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnDockerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
